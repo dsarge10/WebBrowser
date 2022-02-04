80f1ba1ee44e7d351d419f7b425b0ffad5aa0dc2
Dylan Sargent <dds0037@auburn.edu>
2022-02-03 20:28:58 -0600

    Created label and track bar for colorChange

--- WebBrowser.UI/ColorChange.Designer.cs
+++ WebBrowser.UI/ColorChange.Designer.cs
@@ -28,10 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Blue";
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(3, 47);
+            this.tbBlue.Maximum = 255;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(292, 45);
+            this.tbBlue.TabIndex = 1;
+            this.tbBlue.TickFrequency = 5;
+            // 
+            // ColorChange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.label1);
+            this.Name = "ColorChange";
+            this.Size = new System.Drawing.Size(298, 274);
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbBlue;
     }
 }
