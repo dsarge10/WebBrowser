6eb63841a53976775e584f77d0ce1441b499f044
Dylan Sargent <dds0037@auburn.edu>
2022-02-13 15:00:55 -0600

    Created History and Bookmark Manager Forms

---
+++ WebBrowser.UI/Number3.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WebBrowser.UI
+{
+    partial class Number3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(24, 22);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(407, 251);
+            this.listBox2.TabIndex = 0;
+            // 
+            // Number3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.listBox2);
+            this.Name = "Number3";
+            this.Text = "BookmarkManager";
+            this.Load += new System.EventHandler(this.Number3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
---
+++ WebBrowser.UI/Number3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebBrowser.Logic;
+
+namespace WebBrowser.UI
+{
+    public partial class Number3 : Form
+    {
+        public Number3()
+        {
+            InitializeComponent();
+        }
+
+        private void Number3_Load(object sender, EventArgs e)
+        {
+            var items = HistoryManager.GetItems();
+            listBox2.Items.Clear();
+
+            foreach (var item in items)
+            {
+                listBox2.Items.Add(string.Format("{0} - {1} - {2}", item.Title, item.URL));
+            }
+        }
+    }
+}
